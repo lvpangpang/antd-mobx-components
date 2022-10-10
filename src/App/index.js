1a6fcd1c0d0f5077d2eacc734e0d44c2fe925270
lvpangpang <574818075@qq.com>
2022-10-10 14:21:17 +0800

    fix:修复App组件未登录跳转问题

--- src/App/index.js
+++ src/App/index.js
@@ -1,23 +1,31 @@
-import { useEffect } from 'react'
-import { isFun } from 'js-common-library'
-import Loading from './Loading'
-import AppProvider, { useAppInfo } from './context'
+import { useEffect } from "react"
+import { isFun } from "js-common-library"
+import Loading from "./Loading"
+import AppProvider, { useAppInfo } from "./context"
 
 function App(props) {
-  const { id, init, isLogin = () => true, provider = {}, children } = props
+  const {
+    id,
+    init,
+    isLogin = () => true,
+    provider = {},
+    loginUrl = "/login",
+    children,
+  } = props
   useEffect(() => {
     if (isFun(isLogin) && !isLogin()) {
-      return (window.location.href = '/login')
+      return (window.location.href = loginUrl)
     }
     if (isFun(init)) {
       init()
     }
   }, [])
   return (
-    <AppProvider value={provider}>{id ? children : <Loading></Loading>}</AppProvider>
+    <AppProvider value={provider}>
+      {id ? children : <Loading></Loading>}
+    </AppProvider>
   )
 }
 
 export { useAppInfo }
 export default App
-
